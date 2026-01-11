347bc7a1230aebaa95f9918af9262c97f685679e
sujalsoni19 <sujal19soni2004@gmail.com>
2026-01-11 23:43:51 +0530

    upload bug fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,6 +16,8 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 
 app.use(cookieParser());
 
+app.use(express.static("public"));
+
 import userRouter from "./routes/user.route.js";
 
 app.use("/api/users", userRouter);
