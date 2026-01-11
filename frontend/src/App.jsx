271ae8ab92410b1e1e927e9a36d6dc5e72df34b5
sujalsoni19 <sujal19soni2004@gmail.com>
2026-01-11 20:56:51 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-4xl bg-red-400'>Task Management App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/context/Authcontext.js
@@ -0,0 +1,24 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import api from "../api/axios";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    api.get("/api/users/me")
+      .then(res => setUser(res.data.data))
+      .catch(() => setUser(null))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
